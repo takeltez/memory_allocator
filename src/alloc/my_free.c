888e37004ec685796af515900f48ad55ea808712
takeltez <mountain.skier@yandex.ru>
2021-02-16 18:37:38 +0700

    delete old code

--- src/alloc/my_free.c
+++ src/alloc/my_free.c
@@ -24,17 +24,4 @@ void my_free(void *ptr)
 
 		ptr = NULL;
 	}
-	/*for (size_t i = 0; i < call_ind; ++i)
-	{
-		if (chunk_list[i].ptr == ptr - OFFSET_TO_USER_SEG)
-		{
-			*chunk_list[i].is_used = 0;
-
-			 size_t *item = (size_t*)ptr;
-			 
-			 *item = 0;
-
-			 ptr = NULL;
-		}
-	}*/
 }
